18ae249f9ed325cbbcf93567bfcef5e39a16d5a4
Heinz Holzer <holzerheinz@hotmail.com>
2024-09-10 13:44:54 +0200

    Add project files.

---
+++ MauiIssueImageInCollectionView/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiIssueImageInCollectionView
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
